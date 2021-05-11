b1c855774e7e6477c2ccfcc737ad8d726304a95a
Christian Hettlage <hettlage@saao.ac.za>
2021-05-11 15:58:34 +0200

    Fixed unit test

--- frontend/src/app/proposal/proposal.component.spec.ts
+++ frontend/src/app/proposal/proposal.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProposalComponent } from './proposal.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRoute } from '@angular/router';
 
 describe('ProposalComponent', () => {
   let component: ProposalComponent;
@@ -8,9 +10,15 @@ describe('ProposalComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProposalComponent ]
-    })
-    .compileComponents();
+      declarations: [ProposalComponent],
+      imports: [RouterTestingModule],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: { snapshot: { paramMap: { get: () => '2020-2-SCI-043' } } },
+        },
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {

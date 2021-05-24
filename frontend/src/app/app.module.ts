ac25ae10166ccf11d9e8a7b1bbd1158f694b230d
Christian Hettlage <hettlage@saao.ac.za>
2021-05-24 09:37:41 +0200

    Replaced Angular Material with Bulma

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,17 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { MatSliderModule } from '@angular/material/slider';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProposalComponent } from './proposal/proposal.component';
 import { InvestigatorsComponent } from './proposal/investigators/investigators.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
 import { HttpClientModule } from '@angular/common/http';
-import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
 import { ProposalService } from './service/proposal.service';
 import { RealProposalService } from './service/real/real-proposal.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -33,11 +28,6 @@ import { BlockSummariesComponent } from './proposal/block-summaries/block-summar
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    MatSliderModule,
-    MatButtonModule,
-    MatIconModule,
-    MatFormFieldModule,
-    MatSelectModule,
     FormsModule,
     ReactiveFormsModule,
   ],

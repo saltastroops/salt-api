56e0e02c78b5a5fa217df1cc2459e67b78cdfbd3
Chaka Mofokeng <42966715+mofokeng-chaka@users.noreply.github.com>
2022-08-19 13:51:03 +0200

    Minor ng tests updates (#133)

--- src/app/proposal/general/proposal-progress/requested-observing-conditions/requested-observing-conditions.component.spec.ts
+++ src/app/proposal/general/proposal-progress/requested-observing-conditions/requested-observing-conditions.component.spec.ts
@@ -3,6 +3,11 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { RequestedObservingConditionsComponent } from "./requested-observing-conditions.component";
 
 describe("RequestedObservingConditionsComponent", () => {
+  const lastObservingConstraints = {
+    seeing: 1,
+    transparency: "Clear",
+    description: "Description",
+  };
   let component: RequestedObservingConditionsComponent;
   let fixture: ComponentFixture<RequestedObservingConditionsComponent>;
 
@@ -15,6 +20,7 @@ describe("RequestedObservingConditionsComponent", () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(RequestedObservingConditionsComponent);
     component = fixture.componentInstance;
+    component.lastObservingConstraints = lastObservingConstraints;
     fixture.detectChanges();
   });
 

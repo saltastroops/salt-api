e3818c4fc7b359857dd2ac8c7b38b927026fd886
Christian Hettlage <hettlage@saao.ac.za>
2022-08-24 09:57:27 +0200

    Removed test data from BVIT repository tests

--- tests/repository/test_bvit_repository.py
+++ tests/repository/test_bvit_repository.py
@@ -1,23 +1,13 @@
-from typing import Any, Callable
+from typing import Any
 
+import pytest
 from sqlalchemy.engine import Connection
 
 from saltapi.repository.bvit_repository import BvitRepository
 
-TEST_DATA = "repository/bvit_repository.yaml"
 
-
-def test_hrs(db_connection: Connection, testdata: Callable[[str], Any]) -> None:
-    data = testdata(TEST_DATA)["bvit"]
-    for d in data:
-        bvit_id = d["bvit_id"]
-        expected_bvit = d["bvit"]
-        bvit_repository = BvitRepository(db_connection)
-        bvit = bvit_repository.get(bvit_id)
-
-        assert bvit["id"] == bvit_id
-        assert bvit["mode"] == expected_bvit["mode"]
-        assert bvit["filter"] == expected_bvit["filter"]
-        assert bvit["neutral_density"] == expected_bvit["neutral_density"]
-        assert bvit["iris_size"] == expected_bvit["iris_size"]
-        assert bvit["shutter_open_time"] == expected_bvit["shutter_open_time"]
+@pytest.mark.parametrize("bvit_id", [34, 75])
+def test_bvit(bvit_id: int, db_connection: Connection, check_data: Any) -> None:
+    bvit_repository = BvitRepository(db_connection)
+    bvit = bvit_repository.get(bvit_id)
+    check_data(bvit)

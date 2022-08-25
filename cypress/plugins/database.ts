afbc51efce6a05490bb0dbd9450948880d51a5b1
Mofokeng-C <mofokeng.chk@gmail.com>
2022-08-25 12:49:41 +0200

    Minor updates

--- cypress/plugins/database.ts
+++ cypress/plugins/database.ts
@@ -16,7 +16,7 @@ export function updateUserPassword(username: string): Promise<string> {
     const password =
       Math.random().toString(36).substring(2, 15) +
       Math.random().toString(36).substring(2, 15);
-    console.log(mockIntercepts);
+    console.log(recordHttpConfig["mockIntercepts"]);
 
     if (mockIntercepts) {
       resolve(password)

f4aad8e375cf18c612e058c723a75c0bfc7b1a7f
Mofokeng-C <mofokeng.chk@gmail.com>
2022-09-30 13:01:10 +0200

    NIR calibration table

---
+++ src/app/proposal/instruments/nir/nir-calibration/nir-calibration.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NirCalibrationComponent } from './nir-calibration.component';
+
+describe('NirCalibrationComponent', () => {
+  let component: NirCalibrationComponent;
+  let fixture: ComponentFixture<NirCalibrationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NirCalibrationComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NirCalibrationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/proposal/instruments/nir/nir-calibration/nir-calibration.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'wm-nir-calibration',
+  templateUrl: './nir-calibration.component.html',
+  styleUrls: ['./nir-calibration.component.scss']
+})
+export class NirCalibrationComponent {
+  @Input() lamp!: string | null;
+  @Input() filter!: string | null;
+}

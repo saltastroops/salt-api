27ad29083a7f8e892c3e6aa4123561c483ec7586
Mofokeng-C <mofokeng.chk@gmail.com>
2022-09-30 13:20:32 +0200

    Added NIR repository to instrument repository

--- saltapi/repository/instrument_repository.py
+++ saltapi/repository/instrument_repository.py
@@ -4,9 +4,10 @@ from sqlalchemy.engine import Connection
 
 from saltapi.repository.bvit_repository import BvitRepository
 from saltapi.repository.hrs_repository import HrsRepository
+from saltapi.repository.nir_repository import NirRepository
 from saltapi.repository.rss_repository import RssRepository
 from saltapi.repository.salticam_repository import SalticamRepository
-from saltapi.service.instrument import BVIT, HRS, RSS, Salticam
+from saltapi.service.instrument import BVIT, HRS, NIR, RSS, Salticam
 from saltapi.web.schema.rss import RssMaskType
 
 
@@ -16,6 +17,7 @@ class InstrumentRepository:
         self.rss_repository = RssRepository(connection)
         self.hrs_repository = HrsRepository(connection)
         self.bvit_repository = BvitRepository(connection)
+        self.nir_repository = NirRepository(connection)
 
     def get_salticam(self, salticam_id: int) -> Salticam:
         """Return a Salticam setup."""
@@ -33,6 +35,10 @@ class InstrumentRepository:
         """Return a BVIT setup."""
         return self.bvit_repository.get(bvit_id)
 
+    def get_nir(self, nir_id: int) -> NIR:
+        """Return a NIR setup."""
+        return self.nir_repository.get(nir_id)
+
     def get_rss_masks_in_magazine(self, mask_types: List[RssMaskType]) -> List[str]:
         """The list of masks in the magazine."""
         return self.rss_repository.get_mask_in_magazine(mask_types)
@@ -49,6 +55,8 @@ class InstrumentRepository:
         """Update MOS mask metadata"""
         return self.rss_repository.update_mos_mask_metadata(mos_mask_metadata)
 
-    def get_obsolete_rss_masks_in_magazine(self, mask_types: List[RssMaskType]) -> List[str]:
+    def get_obsolete_rss_masks_in_magazine(
+        self, mask_types: List[RssMaskType]
+    ) -> List[str]:
         """The list of obsolete RSS masks."""
         return self.rss_repository.get_obsolete_rss_masks_in_magazine(mask_types)

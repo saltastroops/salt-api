c7e0271e9a7f2256a686173706558192dccdfb73
Mofokeng-C <mofokeng.chk@gmail.com>
2022-10-10 19:16:07 +0200

    Updated NIR schema(s)

--- src/app/types/nir.ts
+++ src/app/types/nir.ts
@@ -1,41 +1,62 @@
 export interface Nir {
   id: number;
   configuration: NirConfiguration;
-  detector: NirDetector;
-  procedure: NirProcedure;
+  procedure: NirProcedure | null;
   observationTime: number;
   overheadTime: number;
 }
 
 export interface NirConfiguration {
-  mode: NirSamplingMode;
-  spectroscopy: NirSpectroscopy | null;
+  grating: string;
+  gratingAngle: number;
+  cameraStation: number;
+  cameraAngle: number;
+  cameraFilterWheel: NirCameraFilterWheel;
   filter: string;
 }
 
 export interface NirDetector {
   mode: NirSamplingMode;
-  resets: number;
   ramps: number;
-  urgGroups: number;
+  groups: number;
   readsPerSample: number;
   exposureTime: number;
   iterations: number;
-  exposureType: string;
-  gain: number;
-}
-
-export interface NirSpectroscopy {
-  grating: string;
-  gratingAngle: number;
-  cameraStation: number;
-  cameraAngle: number;
+  gain: NirGain;
 }
 
 export interface NirProcedure {
   cycles: number;
   procedureType: NirProcedureType;
+  ditherPattern: NirDitherStep[];
 }
+
+export interface NirDitherStep {
+  offset: NirDitherOffset;
+  offsetType: NirOffsetType;
+  detector: NirDetector;
+  exposureType: string;
+}
+
+export interface NirDitherOffset {
+  x: number;
+  y: number;
+}
+
+export type NirGain = "Bright" | "Faint";
+
+export type NirCameraFilterWheel =
+  | "Block"
+  | "Clear"
+  | "Cutoff"
+  | "Diffuser"
+  | "Empty";
+
+export type NirOffsetType =
+  | "FIF Offset"
+  | "Bundle Separation Offset"
+  | "Tracker Guided Offset"
+  | "Unguided Offset";
 
 export type NirSamplingMode = "Normal" | "Focus";
 

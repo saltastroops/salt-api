0e07a05b47a8c5e136f2e19b9e782a2d813f58b3
Mofokeng-C <mofokeng.chk@gmail.com>
2022-10-19 15:48:23 +0200

    Use poetry run command

--- lint.sh
+++ lint.sh
@@ -1,6 +1,6 @@
-black saltapi tests && \
-bandit -r saltapi && \
-isort saltapi tests && \
-flake8 saltapi tests && \
-mypy saltapi && \
-pytest
+poetry run black saltapi tests && \
+poetry run bandit -r saltapi && \
+poetry run isort saltapi tests && \
+poetry run flake8 saltapi tests && \
+poetry run mypy saltapi && \
+poetry run pytest

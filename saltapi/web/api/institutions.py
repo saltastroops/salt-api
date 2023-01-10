9263c5bd1f29902e74638da5d230c9cbc60a9299
Mofokeng-C <mofokeng.chk@gmail.com>
2023-01-10 11:58:06 +0200

    Create institution endpoint

--- saltapi/web/api/institutions.py
+++ saltapi/web/api/institutions.py
@@ -1,8 +1,11 @@
 from typing import Any, Dict, List
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Body
+from starlette import status
 
 from saltapi.repository.unit_of_work import UnitOfWork
+from saltapi.service.institution import NewInstitutionDetails
+from saltapi.service.institution import Institution as _NewInstitutionDetails
 from saltapi.web import services
 from saltapi.web.schema.institution import Institution
 
@@ -18,3 +21,25 @@ def get_institutions() -> List[Dict[str, Any]]:
     with UnitOfWork() as unit_of_work:
         institution_service = services.institution_service(unit_of_work.connection)
         return institution_service.get_institutions()
+
+
+@router.post(
+    "/",
+    summary="Create an institution",
+    status_code=status.HTTP_201_CREATED,
+)
+def create_institution(
+        institution: NewInstitutionDetails = Body(
+            ...,
+            title="Institution details",
+            description="Institution details for the institution to create."
+        )
+) -> _NewInstitutionDetails:
+    with UnitOfWork() as unit_of_work:
+        institution_service = services.institution_service(unit_of_work.connection)
+        institution_service.create(institution)
+
+        unit_of_work.commit()
+
+        return institution_service.get_institution_by_name(institution.institution_name)
+

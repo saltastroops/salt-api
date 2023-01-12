c219b2ff1dee65ae6f1ef7222c5a81fdd00aa741
Mofokeng-C <mofokeng.chk@gmail.com>
2023-01-12 11:02:25 +0200

    Review fix: use random institution name and address in a test

--- tests/integration/institutions/test_create_institution.py
+++ tests/integration/institutions/test_create_institution.py
@@ -1,21 +1,20 @@
-from typing import Any, Callable
-
+from faker import Faker
 from fastapi.testclient import TestClient
 from starlette import status
 
 INSTITUTION_URL = "/institutions"
 
 
-def test_create_institution(
-    client: TestClient, check_data: Callable[[Any], None]
-) -> None:
+def test_create_institution(client: TestClient) -> None:
+    fake = Faker()
     institution = {
-        "institution_name": "World University",
+        "institution_name": f"{fake.name()} University",
         "department": "Physics",
-        "address": "1 Earth Street\nEarth\n0000",
+        "address": fake.address(),
         "url": "www.world-university.com",
     }
     response = client.post(INSTITUTION_URL + "/", json=institution)
+    new_institution = response.json()
 
     assert response.status_code == status.HTTP_201_CREATED
     check_data(response.json())

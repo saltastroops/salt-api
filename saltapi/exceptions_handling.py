a7295b8d17f762e4abfd91bcd75a96cdee03fa17
Mofokeng-C <mofokeng.chk@gmail.com>
2023-01-16 07:26:15 +0200

    Code reformatting

--- saltapi/exceptions_handling.py
+++ saltapi/exceptions_handling.py
@@ -15,10 +15,10 @@ from saltapi.exceptions import AuthorizationError, NotFoundError
 def log_message(method: str, url: Union[str, URL], message: Any):
     """log message when catch exception"""
 
-    logger.error('start error, this is'.center(60, '*'))
-    logger.error(f'{method} {url}')
+    logger.error("start error, this is".center(60, "*"))
+    logger.error(f"{method} {url}")
     logger.error(message)
-    logger.error('end error'.center(60, '*'))
+    logger.error("end error".center(60, "*"))
 
 
 def setup_exception_handler(app: FastAPI):
@@ -27,38 +27,56 @@ def setup_exception_handler(app: FastAPI):
         """catch FastAPI HTTPException"""
 
         log_message(request.method, request.url, exc.detail)
-        return JSONResponse(content={"message": exc.detail}, status_code=exc.status_code, headers=exc.headers)
+        return JSONResponse(
+            content={"message": exc.detail},
+            status_code=exc.status_code,
+            headers=exc.headers,
+        )
 
     @app.exception_handler(RequestValidationError)
-    async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    async def validation_exception_handler(
+        request: Request, exc: RequestValidationError
+    ):
         """catch FastAPI RequestValidationError"""
 
-        exc_str = f'{exc}'.replace('\n', ' ').replace('   ', ' ')
+        exc_str = f"{exc}".replace("\n", " ").replace("   ", " ")
         log_message(request.method, request.url, exc)
-        return JSONResponse(content={"message": exc_str}, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+        return JSONResponse(
+            content={"message": exc_str},
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        )
 
     @app.exception_handler(Exception)
     async def exception_handle(request: Request, exc: Exception):
         """catch other exception"""
 
         log_message(request.method, request.url, traceback.format_exc())
-        return JSONResponse(content={"message": str(exc)}, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return JSONResponse(
+            content={"message": str(exc)},
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
 
     @app.exception_handler(NotFoundError)
     async def not_found_error_handler(request: Request, exc: NotFoundError) -> Response:
         log_message(request.method, request.url, exc)
-        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={"message": "Not Found"})
-
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND, content={"message": "Not Found"}
+        )
 
     @app.exception_handler(ValidationError)
-    async def validation_error_handler(request: Request, exc: ValidationError) -> Response:
+    async def validation_error_handler(
+        request: Request, exc: ValidationError
+    ) -> Response:
         log_message(request.method, request.url, exc)
-        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={"message": str(exc)})
-
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND, content={"message": str(exc)}
+        )
 
     @app.exception_handler(AuthorizationError)
     async def authorization_error_handler(
-            request: Request, exc: AuthorizationError
+        request: Request, exc: AuthorizationError
     ) -> Response:
         log_message(request.method, request.url, exc)
-        return JSONResponse(status_code=status.HTTP_403_FORBIDDEN, content={"message": "Forbidden"})
+        return JSONResponse(
+            status_code=status.HTTP_403_FORBIDDEN, content={"message": "Forbidden"}
+        )

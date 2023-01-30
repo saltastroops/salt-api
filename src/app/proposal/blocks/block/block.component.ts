a15690868dd279a7badb3de13128ce6d229d1dad
Mofokeng-C <mofokeng.chk@gmail.com>
2023-01-30 11:33:17 +0200

    Add necessary methods

--- src/app/proposal/blocks/block/block.component.ts
+++ src/app/proposal/blocks/block/block.component.ts
@@ -1,6 +1,10 @@
 import { Component, Input } from "@angular/core";
 
 import { Block } from "../../../types/block";
+import {parseISO} from "date-fns";
+import {FinderChart} from "../../../types/observation";
+import {finderChartURL} from "../../../util";
+import {environment} from "../../../../environments/environment";
 
 @Component({
   selector: "wm-block",
@@ -9,4 +13,26 @@ import { Block } from "../../../types/block";
 })
 export class BlockComponent {
   @Input() block!: Block;
+  firstValidFrom: Date = parseISO("1970-01-01T00:00:00Z");
+  lastValidUntil: Date = parseISO("2100-01-01T00:00:00Z");
+
+  thumbnailFinderChartURL(finderChart: FinderChart): string {
+    const url = finderChartURL(
+      finderChart,
+      ["thumbnail", "original"],
+      ["png", "jpg"],
+      environment.apiUrl,
+    );
+    return url || "/assets/noun-missing-2181345.png";
+  }
+
+  originalFinderChartURL(finderChart: FinderChart): string {
+    const url = finderChartURL(
+      finderChart,
+      ["original"],
+      ["pdf", "png", "jpg"],
+      environment.apiUrl,
+    );
+    return url || "/assets/noun-missing-2181345.png";
+  }
 }

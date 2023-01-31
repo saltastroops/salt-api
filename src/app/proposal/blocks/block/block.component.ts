9fb79a0af90b3a4a5f050f9bab96f1b3c1c29e74
Christian Hettlage <hettlage@saao.ac.za>
2023-01-31 15:22:42 +0200

    Show only finder charts for the current Julian day

--- src/app/proposal/blocks/block/block.component.ts
+++ src/app/proposal/blocks/block/block.component.ts
@@ -5,7 +5,7 @@ import { parseISO } from "date-fns";
 import { environment } from "../../../../environments/environment";
 import { Block } from "../../../types/block";
 import { FinderChart } from "../../../types/observation";
-import { finderChartURL } from "../../../util";
+import { currentJulianDay, finderChartURL } from "../../../util";
 
 @Component({
   selector: "wm-block",
@@ -14,12 +14,26 @@ import { finderChartURL } from "../../../util";
 })
 export class BlockComponent implements OnInit {
   @Input() block!: Block;
-  firstValidFrom: Date | undefined;
-  lastValidUntil: Date | undefined;
+
+  validFrom!: Date;
+
+  validUntil!: Date;
 
   ngOnInit(): void {
-    this.firstValidFrom = parseISO("1970-01-01T00:00:00Z");
-    this.lastValidUntil = parseISO("2100-01-01T00:00:00Z");
+    const jd = currentJulianDay();
+    this.validFrom = jd.start;
+    this.validUntil = jd.end;
+  }
+
+  validSortedFinderCharts(finderCharts: FinderChart[]): FinderChart[] {
+    const isValid = (fc: FinderChart) =>
+      (fc.validFrom === null || parseISO(fc.validFrom) <= this.validUntil) &&
+      (fc.validUntil === null || parseISO(fc.validUntil) >= this.validFrom);
+    const millisecondsFromEpoch = (fc: FinderChart) =>
+      fc.validFrom ? parseISO(fc.validFrom).getTime() : 0;
+    const compare = (fc1: FinderChart, fc2: FinderChart) =>
+      millisecondsFromEpoch(fc1) - millisecondsFromEpoch(fc2);
+    return [...finderCharts.filter(isValid)].sort(compare);
   }
 
   thumbnailFinderChartURL(finderChart: FinderChart): string {

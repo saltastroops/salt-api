177168a1ee3ec45800790ff6cf59ef0164ed596d
Mofokeng-C <mofokeng.chk@gmail.com>
2023-03-16 18:24:53 +0200

    Rview fix: parametrize test parameters

--- tests/integration/proposals/test_update_investigator_proposal_approval_status.py
+++ tests/integration/proposals/test_update_investigator_proposal_approval_status.py
@@ -129,18 +129,26 @@ def test_investigator_approval_proposal_status_update_forbids_a_permitted_user_f
     assert response.status_code == status.HTTP_403_FORBIDDEN
 
 
-def test_investigator_approval_proposal_status_update_for_an_administrator(
+@pytest.mark.parametrize(
+    "username, investigator_id, approved",
+    [
+        (find_username("Administrator"), 1413, True),
+        (find_username("Investigator", proposal_code="2019-2-SCI-006"), 656, False),
+    ],
+)
+def test_investigator_approval_proposal_status_update(
+    username: str,
+    investigator_id: int,
+    approved: bool,
     client: TestClient,
 ) -> None:
     proposal_code = "2019-2-SCI-006"
-    username = find_username("Administrator")
     authenticate(username, client)
 
-    user_id = 1413  # user id for the Principal Contact user in proposal 2019-2-SCI-006
-    data = {"approved": True}
+    data = {"approved": approved}
 
     response = client.put(
-        f"{PROPOSALS_URL}/{proposal_code}/approvals/{user_id}",
+        f"{PROPOSALS_URL}/{proposal_code}/approvals/{investigator_id}",
         json=data,
     )
     assert response.status_code == status.HTTP_200_OK
@@ -151,37 +159,8 @@ def test_investigator_approval_proposal_status_update_for_an_administrator(
 
     proposal = resp.json()
 
-    investigator = [i for i in proposal["investigators"] if i["id"] == user_id]
+    investigators = [i for i in proposal["investigators"] if i["id"] == investigator_id]
 
-    has_approved_proposal = investigator[0]["has_approved_proposal"]
+    has_approved_proposal = investigators[0]["has_approved_proposal"]
 
-    assert has_approved_proposal
-
-
-def test_investigator_approval_proposal_status_update_for_an_investigator(
-    client: TestClient,
-) -> None:
-    proposal_code = "2019-2-SCI-006"
-    username = find_username("Investigator", proposal_code="2019-2-SCI-006")
-    authenticate(username, client)
-
-    user_id = 656  # user id for investigator in proposal 2019-2-SCI-006
-    data = {"approved": False}
-
-    response = client.put(
-        f"{PROPOSALS_URL}/{proposal_code}/approvals/{user_id}",
-        json=data,
-    )
-    assert response.status_code == status.HTTP_200_OK
-
-    resp = client.get(f"{PROPOSALS_URL}/{proposal_code}")
-
-    assert resp.status_code == status.HTTP_200_OK
-
-    proposal = resp.json()
-
-    investigator = [i for i in proposal["investigators"] if i["id"] == user_id]
-
-    has_approved_proposal = investigator[0]["has_approved_proposal"]
-
-    assert not has_approved_proposal
+    assert has_approved_proposal if approved else not has_approved_proposal

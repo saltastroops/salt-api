b612241252249da289ef288c0a33b97526e52c5d
Christian Hettlage <hettlage@saao.ac.za>
2023-03-29 17:15:28 +0200

    Run flake8

--- jenkins/run-tests.sh
+++ jenkins/run-tests.sh
@@ -9,3 +9,5 @@ source /venv/bin/activate
 poetry export -f requirements.txt --output requirements.txt --without-hashes
 python -m pip install -r requirements.txt
 
+# Run the tests
+flake8 --exit-zero saltapi tests

66961af5a9b548f948c966a108b9b9d64d2d4612
Christian Hettlage <hettlage@saao.ac.za>
2023-03-29 17:05:19 +0200

    Add an initial script for running tests

---
+++ jenkins/run-tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Create and activate the virtual environment
+# /venv should be a Docker volume to avoid installing libraries on every pipeline run
+python -m venv /venv
+source /venv/bin/activate
+
+# Gather the requirements and install them
+poetry export -f requirements.txt --output requirements.txt --without-hashes
+python -m pip install -r requirements.txt
+

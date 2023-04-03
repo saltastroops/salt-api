6012569dbf4cb1d6f7c2a320853222ead9262f3f
Christian Hettlage <hettlage@saao.ac.za>
2023-04-03 17:50:42 +0200

    Include dev dependencies in requirements

--- jenkins/run-tests.sh
+++ jenkins/run-tests.sh
@@ -6,13 +6,9 @@ python -m venv /venv
 source /venv/bin/activate
 
 # Gather the requirements and install them
-poetry export -f requirements.txt --output requirements.txt --without-hashes
+poetry export -f requirements.txt --output requirements.txt --without-hashes --with dev
 python -m pip install wheel
 python -m pip install -r requirements.txt
-python -m pip install flake8
-python -m pip install isort
-python -m pip install mypy
-python -m pip install pytest
 
 # Run the tests
 black --check || true

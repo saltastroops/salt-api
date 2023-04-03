03bed6e763d6775027f9d7b752b62ae57b27b0fd
Christian Hettlage <hettlage@saao.ac.za>
2023-04-03 13:26:37 +0200

    Install development tools in virtual environment

--- jenkins/run-tests.sh
+++ jenkins/run-tests.sh
@@ -7,7 +7,12 @@ source /venv/bin/activate
 
 # Gather the requirements and install them
 poetry export -f requirements.txt --output requirements.txt --without-hashes
+python -m pip install wheel
 python -m pip install -r requirements.txt
+python -m pip install flake8
+python -m pip install isort
+python -m pip install mypy
+python -m pip install pytest
 
 # Run the tests
 black --check || true

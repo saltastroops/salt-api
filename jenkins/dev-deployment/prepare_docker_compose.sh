63c052902d22a51f605ea823aa1d3f535bee683e
Christian Hettlage <hettlage@saao.ac.za>
2023-04-19 14:13:29 +0200

    Debug

--- jenkins/dev-deployment/prepare_docker_compose.sh
+++ jenkins/dev-deployment/prepare_docker_compose.sh
@@ -4,7 +4,8 @@
 # the generated file.
 
 # Generate a pseudo-unique file path with the current Unix timestamp.
-tmp_file="docker-compose.$(date +%s).yml"
+# tmp_file="docker-compose.$(date +%s).yml"
+tmp_file="ocker-compose.yml"
 
 # Replace the ${DOCKER_REGISTRY} and ${TAG} environment variables in the docker
 # compose file with the correct values, and save the result to the temporary file.

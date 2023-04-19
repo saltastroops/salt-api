f1f3f9c2aa808c4b64476ef99d046d58684440ee
Christian Hettlage <hettlage@saao.ac.za>
2023-04-19 19:34:09 +0200

    Write temporary file in Jenkinsfile

--- jenkins/dev-deployment/prepare_docker_compose.sh
+++ jenkins/dev-deployment/prepare_docker_compose.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-# Prepare the docker compose file, save it as a temporary file and output the path of
-# the generated file.
-
-# Generate a pseudo-unique file path with the current Unix timestamp.
-# tmp_file="docker-compose.$(date +%s).yml"
-tmp_file="docker-compose.dev.yml"
+# Prepare the docker compose file and output the content of the generated file.
 
 # Replace the ${DOCKER_REGISTRY} and ${TAG} environment variables in the docker
 # compose file with the correct values, and save the result to the temporary file.
 # Note that the pound sign is used as the delimiter for the first sed pattern, as the
-# reoplacement value (a URL) contains slashes.
+# replacement value (a URL) contains slashes.
 registry_pattern='s#${DOCKER_REGISTRY}#'
 registry_pattern+="${DOCKER_REGISTRY}#"
-sed "$registry_pattern" docker-compose.yml | sed 's/${TAG}/dev/' > "$tmp_file"
-
-# Output the file path of the temporary file so that it can be used by the Jenkinsfile
-echo "$tmp_file"
+sed "$registry_pattern" docker-compose.yml | sed 's/${TAG}/dev/'

e21d9dca0ef7d5b11351d9dfd493eb57bfc9a61e
Mofokeng-C <mofokeng.chk@gmail.com>
2023-05-11 11:53:20 +0200

    Review fix: add proposal_view_non_grantee username

--- tests/repository/test_user_repository.py
+++ tests/repository/test_user_repository.py
@@ -331,11 +331,11 @@ def test_has_proposal_permission_returns_correct_result(
         proposal_code=proposal_code,
     )
 
-    non_grantee_username = find_usernames("proposal_view_grantee", True, proposal_code)[
+    non_grantee_username = find_usernames("proposal_view_non_grantee", True, proposal_code)[
         0
     ]
     non_grantee_id = user_repository.get_by_username(non_grantee_username).id
-    assert user_repository.user_has_proposal_permission(
+    assert not user_repository.user_has_proposal_permission(
         user_id=non_grantee_id,
         permission_type="View",
         proposal_code=proposal_code,

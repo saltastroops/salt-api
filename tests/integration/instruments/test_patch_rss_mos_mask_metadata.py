df9418f15c8520e411876e2886be2ff701199dd1
Mofokeng-C <mofokeng.chk@gmail.com>
2023-06-26 11:20:23 +0200

    Review fix: add more tests

--- tests/integration/instruments/test_patch_rss_mos_mask_metadata.py
+++ tests/integration/instruments/test_patch_rss_mos_mask_metadata.py
@@ -1,4 +1,6 @@
 import pytest
+import pytz
+from datetime import date
 from fastapi.testclient import TestClient
 from starlette import status
 
@@ -57,7 +59,7 @@ def test_mos_mask_update_requires_requires_permissions(
     assert response.status_code == status.HTTP_403_FORBIDDEN
 
 
-def test_mos_mask_update(
+def test_mos_mask_update_cut_by(
     client: TestClient,
 ) -> None:
     barcode = "P000127N01"
@@ -76,3 +78,26 @@ def test_mos_mask_update(
     assert metadata["cut_by"] == cut_by
     assert metadata["cut_date"] is None
     assert metadata["mask_comment"] is None
+
+
+def test_mos_mask_update_cut_date_and_mask_comment(
+        client: TestClient,
+) -> None:
+    barcode = "P000127N01"
+    cut_by = "Chaka"
+    cut_date = date.today().isoformat()
+    mask_comment = "Mask re-cut, first one bad."
+
+    user = find_username("SALT Astronomer")
+    authenticate(user, client)
+    response = client.patch(
+        RSS_MOS_MASK_METADATA_URL + "/" + barcode,
+        json={"cut_by": cut_by, "cut_date": cut_date, "mask_comment": mask_comment},
+        )
+    assert response.status_code == status.HTTP_200_OK
+
+    metadata = response.json()
+    assert metadata["barcode"] == barcode
+    assert metadata["cut_by"] == cut_by
+    assert metadata["cut_date"] == cut_date
+    assert metadata["mask_comment"] == mask_comment

1d559c23fd1dee81083716883bcc48aab5422eaa
mofokeng-chaka <mofokeng.chk@gmail.com>
2023-08-30 10:14:06 +0200

    Add more tests

--- tests/integration/proposals/test_get_proposal_status.py
+++ tests/integration/proposals/test_get_proposal_status.py
@@ -16,6 +16,17 @@ def test_should_return_401_when_requesting_proposal_status_for_unauthenticated_u
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
 
+def test_should_return_422_when_requesting_proposal_status_with_a_wrong_proposal_code_format(
+        client: TestClient,
+) -> None:
+    proposal_code = "20a1-1-ABC-123"
+
+    user = find_username("Administrator")
+    authenticate(user, client)
+    response = client.get(PROPOSALS_URL + "/" + proposal_code + "/status")
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
 def test_should_return_404_when_requesting_proposal_status_of_non_existing_proposal(
     client: TestClient,
 ) -> None:

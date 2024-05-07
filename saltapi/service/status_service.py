507439eb80454eb3b213773678c3085a8d8d3a9f
Christian Hettlage <hettlage@saao.ac.za>
2024-05-07 14:12:44 +0200

    Send an email when the telescope status is updated
    The implementation is inefficient, and should only be used for a handful of emails.

--- saltapi/service/status_service.py
+++ saltapi/service/status_service.py
@@ -1,9 +1,12 @@
-from typing import List
+from datetime import datetime
+from typing import Any, Dict, List, Tuple
 
 from saltapi.repository.status_repository import (
     StatusRepository,
     SubsystemStatusDetails,
 )
+from saltapi.service.mail_service import MailService
+from saltapi.settings import get_settings
 from saltapi.web.schema.status import SubsystemStatusUpdate
 
 
@@ -24,3 +27,60 @@ class StatusService:
             "subsystem": subsystem_status_update.subsystem,
         }
         self.status_repository.update_status(subsystem_status_details)
+
+        self._send_email_notification(subsystem_status_details)
+
+    @staticmethod
+    def _send_email_notification(status_details: Dict[str, Any]):
+        # Get the list of recipients
+        recipients_string = get_settings().status_update_email_recipients
+        recipients = [r.strip() for r in recipients_string.split(",")]
+
+        # Generate the email text
+        plain, html = StatusService._status_email_text(status_details)
+
+        # Send an email to all the recipients
+        for recipient in recipients:
+            message = MailService.generate_email(
+                to=recipient,
+                subject="SALT status update",
+                html_body=html,
+                plain_body=plain,
+            )
+            MailService.send_email(message=message, to=[recipient])
+
+    @staticmethod
+    def _status_email_text(status_details: Dict[str, Any]) -> Tuple[str, str]:
+        if status_details["subsystem"].lower() == "telescope":
+            subsystem = "The telescope"
+        else:
+            subsystem = status_details["subsystem"]
+        html = f"""\
+<p>Good day,</p>
+
+<p>The status of SALT has changed as follows:</p>
+
+<p>{subsystem} is {status_details["status"].lower()}.
+"""
+        if status_details["reason"]:
+            html += f"""\
+<br>Reason: {status_details["reason"]}            
+        """
+        html += "</p>"
+        eta: datetime = status_details["expected_available_again_at"]
+        if eta:
+            day = eta.strftime("%d").lstrip("0")
+            html += f"""\
+
+<p>{subsystem} is expected to be available again on {day} {eta.strftime("%B %Y")} at {eta.strftime("%X")} UTC.</p>
+                    """
+        html += """\
+
+<p>Kind regards,</p>
+
+<p>SALT Astronomy Operations</p>
+"""
+
+        plain = html.replace("<p>", "").replace("</p>", "").replace("<br>", "")
+
+        return plain, html

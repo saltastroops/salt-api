3b79576043a22afc4121a1443ae5f072946fc2ec
Christian Hettlage <hettlage@saao.ac.za>
2024-05-08 12:02:28 +0200

    Separate updating the status and sending the email

--- saltapi/service/status_service.py
+++ saltapi/service/status_service.py
@@ -18,26 +18,41 @@ class StatusService:
         return self.status_repository.get_status()
 
     def add_status_update(self, subsystem_status_update: SubsystemStatusUpdate) -> None:
-        subsystem_status_details: SubsystemStatusDetails = {
-            "expected_available_again_at": subsystem_status_update.expected_available_again_at,
-            "reason": subsystem_status_update.reason,
-            "reporting_user": subsystem_status_update.reporting_user,
-            "status": subsystem_status_update.status.value,
-            "status_changed_at": subsystem_status_update.status_changed_at,
-            "subsystem": subsystem_status_update.subsystem,
-        }
+        """
+        Add the status update for a subsystem to the database.
+
+        Parameters
+        ----------
+        subsystem_status_update
+            Status update for a subsystem.
+
+        """
+        subsystem_status_details = self._subsystem_status_details(
+            subsystem_status_update
+        )
         self.status_repository.update_status(subsystem_status_details)
 
-        self._send_email_notification(subsystem_status_details)
+    def send_status_update_email(
+        self, subsystem_status_update: SubsystemStatusUpdate
+    ) -> None:
+        """
+        Send out an email about the status update for a subsystem.
 
-    @staticmethod
-    def _send_email_notification(status_details: Dict[str, Any]):
+        Parameters
+        ----------
+        subsystem_status_update
+            Status update for a subsystem.
+
+        """
         # Get the list of recipients
         recipients_string = get_settings().status_update_email_recipients
         recipients = [r.strip() for r in recipients_string.split(",")]
 
         # Generate the email text
-        plain, html = StatusService._status_email_text(status_details)
+        subsystem_status_details = self._subsystem_status_details(
+            subsystem_status_update
+        )
+        plain, html = StatusService._status_email_text(subsystem_status_details)
 
         # Send an email to all the recipients
         for recipient in recipients:
@@ -48,6 +63,18 @@ class StatusService:
                 plain_body=plain,
             )
             MailService.send_email(message=message, to=[recipient])
+
+    def _subsystem_status_details(
+        self, subsystem_status_update: SubsystemStatusUpdate
+    ) -> SubsystemStatusDetails:
+        return {
+            "expected_available_again_at": subsystem_status_update.expected_available_again_at,
+            "reason": subsystem_status_update.reason,
+            "reporting_user": subsystem_status_update.reporting_user,
+            "status": subsystem_status_update.status.value,
+            "status_changed_at": subsystem_status_update.status_changed_at,
+            "subsystem": subsystem_status_update.subsystem,
+        }
 
     @staticmethod
     def _status_email_text(status_details: Dict[str, Any]) -> Tuple[str, str]:

f4fcd15b8cb172a22ec78f81a5ce41fe29ccce0f
mofokeng-chaka <mofokeng.chk@gmail.com>
2024-07-23 08:56:55 +0200

    Minor updates

--- saltapi/service/authentication_service.py
+++ saltapi/service/authentication_service.py
@@ -116,9 +116,9 @@ def _user_from_auth_header(authorization: str, verification: bool = False) -> Us
 def _user_from_session(request: Request) -> User:
     user_id = request.session.get(USER_ID_KEY)
 
-    singed_auth_token = request.cookies.get(SECONDARY_AUTH_TOKEN_KEY)
+    signed_auth_token = request.cookies.get(SECONDARY_AUTH_TOKEN_KEY)
     secondary_auth_token = jwt.decode(
-        singed_auth_token, SECRET_KEY, algorithms=[ALGORITHM]
+        signed_auth_token, SECRET_KEY, algorithms=[ALGORITHM]
     )
 
     signed_auth_token_from_session = request.session.get(SECONDARY_AUTH_TOKEN_KEY)

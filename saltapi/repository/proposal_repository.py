4a68f9b98186e7cf2ab01c940e7ff065734a419a
Eb-Zeero <macebelene@gmail.com>
2025-09-25 16:47:37 +0200

    Remove double named method Make proprietary period none Optional

--- saltapi/repository/proposal_repository.py
+++ saltapi/repository/proposal_repository.py
@@ -514,46 +514,8 @@ LIMIT 1;
 
     def _proposal_general_info(self, proposal_code: str, semester: str):
         """
-        Return the proposal text for a semester.
-
-        The proposal text includes the title, abstract, summary for the SALT Astronomer and the summary for the night
-        log.
-
-        No text may exist for the given semester as no phase 2 has been submitted for the proposal yet.
-        In this case the latest text (preceding the semester) is used.
-        """
-        stmt = text(
-            """
-SELECT
-    PT.Title                            AS title,
-    PT.Abstract                         AS abstract,
-    PT.ReadMe                           AS summary_for_salt_astronomer,
-    PT.NightlogSummary                  AS summary_for_night_log
-FROM ProposalText PT
-         JOIN ProposalCode PC ON PT.ProposalCode_Id = PC.ProposalCode_Id
-         JOIN Semester S ON PT.Semester_Id = S.Semester_Id
-WHERE PC.Proposal_Code = :proposal_code
-  AND CONCAT(S.Year, '-', S.Semester) <= :semester
-ORDER BY S.Year, S.Semester DESC
-LIMIT 1;
-        """
-        )
-        result = self.connection.execute(
-            stmt, {"proposal_code": proposal_code, "semester": semester}
-        )
-        row = result.one()
-        return {
-            "title": row.title,
-            "abstract": row.abstract,
-            "summary_for_salt_astronomer": row.summary_for_salt_astronomer,
-            "summary_for_night_log": row.summary_for_night_log,
-        }
-
-    def _proposal_general_info(self, proposal_code: str, semester: str):
-        """
         Return general proposal information for a semester.
         """
-        year, sem = semester.split("-")
         stmt = text(
             """
 SELECT 
